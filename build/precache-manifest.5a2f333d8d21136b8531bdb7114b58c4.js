7bc7756c58b835119a3d7dd11dc3ae5df3bab668
mindu902 <96086388+mindu902@users.noreply.github.com>
2023-02-02 15:07:51 -0800

    edit

---
+++ build/precache-manifest.5a2f333d8d21136b8531bdb7114b58c4.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "62a22fd63ea0d0e1b59bf0b89b3a1ee2",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5bc25090477de51735f5",
+    "url": "/static/css/2.d64471e2.chunk.css"
+  },
+  {
+    "revision": "f5a427861efb069de60f",
+    "url": "/static/css/main.db6f0745.chunk.css"
+  },
+  {
+    "revision": "5bc25090477de51735f5",
+    "url": "/static/js/2.1fd00cd6.chunk.js"
+  },
+  {
+    "revision": "7c4b6536cab2d1970758ff058c5fbbf8",
+    "url": "/static/js/2.1fd00cd6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f5a427861efb069de60f",
+    "url": "/static/js/main.25962e40.chunk.js"
+  },
+  {
+    "revision": "a2d41c56873c741b51b2",
+    "url": "/static/js/runtime-main.a5042f6b.js"
+  }
+]);
